b7c21fa2632d71b90fa596080e77b4dc43f47416
Nikolya11d <horoman8@gmail.com>
2023-12-02 00:37:37 +0300

    laba2
    first

---
+++ search.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"func.h"
+void search(world *arr, int n)
+{
+	int  rare;
+	printf("1-ПОИСК ПО СТРАНЕ,2-ПОИСК ПО НАСЕЛЕНИЮ,3-ПОИСК ПО ПЛОЩАДИ\n");
+	scanf("%d",&rare);
+	switch(rare)
+	{
+		case 1:
+		{
+			int key =1;
+			char countryc[40];
+			printf("ВВЕДИ НАЗВАНИЕ СТРАНЫ\n");
+			scanf("%s",countryc);
+			for (int i=0;i<n;i++)
+			{
+				if(!strcmp(arr[i].country,countryc))
+				{
+					printf("%s %i %f\n",arr[i].country,arr[i].population,arr[i].square);
+					key=0;
+				}
+			}
+			if(key){
+				printf("НИЧЕГО НЕ НАШЛОСЬ\n");
+				return;
+				};
+			break;
+		}
+		case 2:
+                {
+                        int key =1;
+                        int populationc=0;
+                        printf("ВВЕДИ КОЛИЧЕСТВО НАСЕЛЕНИЯ СТРАНЫ\n");
+                        scanf("%d",&populationc);
+                        for (int i=0;i<n;i++)
+                        {
+                                if(arr[i].population==populationc)
+                                {
+                                        printf("%s %i %f\n",arr[i].country,arr[i].population,
+arr[i].square);
+                                        key=0;
+                                }
+                        }
+                        if(key){
+                                printf("НИЧЕГО НЕ НАШЛОСЬ\n");
+				return;
+                                }
+                        break;
+                }
+		case 3:
+                {
+                        int key =1;
+                        float squarec=0;
+                        printf("ВВЕДИ ПЛОЩАДЬ СТРАНЫ\n");
+                        scanf("%f",&squarec);
+                        for (int i=0;i<n;i++)
+                        {
+                                if(arr[i].square==squarec)
+                                {
+                                        printf("%s %i %f\n",arr[i].country,arr[i].population,arr[i].square);
+                                        key=0;
+                                }
+                        }
+                        if(key) printf("НИЧЕГО НЕ НАШЛОСЬ\n");
+				return;
+                        break;
+                }
+		default:
+			break;
+	}
+
+}
